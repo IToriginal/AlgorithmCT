0d8e7056745cb07480c5b3270eceb7ce4b764555
IToriginal <gitshineit@gmail.com>
2023-01-08 21:55:35 +0900

    2023-01-08 BaekJoon 코딩테스트 - 사칙연산 No.1000: 덧셈(+) No.1001: 뺄셈(-) No.1008: 나눗셈(/) No.10998: 곱셈(*)

---
+++ src/N1008.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class N1008 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double A = scanner.nextDouble();
+        double B = scanner.nextDouble();
+        System.out.println(A/B);
+        scanner.close();
+    }
+}
