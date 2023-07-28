935aa900a117606f1020e358bf8842cd1d240e4c
IToriginal <gitshineit@gmail.com>
2023-07-28 22:09:29 +0900

    :fire: remove: 문제 정리

--- src/backstep1/N2557.java
+++
@@ -1,9 +0,0 @@
-//입출력과 사칙연산
-package backstep1;
-
-public class N2557 {
-    public static void main(String[] args) {
-        //"Hello World!" 출력하기
-        System.out.println("Hello World!");
-    }
-}
--- src/backstep6/N11654.java
+++
@@ -1,8 +0,0 @@
-package backstep6;
-
-public class N11654 {
-    public static void main(String[] args) throws Exception{
-        int a = System.in.read();
-        System.out.print(a);
-    }
-}
