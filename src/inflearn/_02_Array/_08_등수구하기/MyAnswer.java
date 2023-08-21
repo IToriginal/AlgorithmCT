4672dc39b5426fd2afc64c248d77bf6f0464d6f0
IToriginal <gitshineit@gmail.com>
2023-08-21 21:11:30 +0900

    2023. 08. 21 (월)
    ✅ Inflearn: Array - 6 문제 (2 / 6)
    ❌ No.9: 격자판최대합
    ❌ No.10: 봉우리
    ❌ No.11: 임시반장정하기
    ❌ No.12: 멘토링

---
+++ src/inflearn/_02_Array/_08_등수구하기/MyAnswer.java
@@ -0,0 +1,42 @@
+package inflearn._02_Array._08_등수구하기;
+
+import java.io.*;
+import java.util.Stack;
+
+public class MyAnswer {
+    public int[] solution(int N, String s) {
+        Stack<Integer> stack = new Stack<>();
+        String[] split = s.split(" ");
+
+        int[] scores = new int[N];
+        for (int i = 0; i < N; i++) {
+            scores[i] = Integer.parseInt(split[i]);
+            stack.push(scores[i]);
+        }
+
+        int[] rank = new int[N];
+        for (int i = 0; i < N; i++) {
+            int count = 1;
+            int tmp = stack.peek();
+            stack.pop();
+            for (int j = 0; j < N; j++) {
+                if (tmp < scores[j]) {
+                    count++;
+                }
+            }
+            rank[i] = count;
+        }
+        return rank;
+    }
+
+    public static void main(String[] args) throws IOException {
+        MyAnswer T = new MyAnswer();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+        int[] result = T.solution(N, s);
+        for (int i = result.length - 1; i >= 0; i--) {
+            System.out.print(result[i] + " ");
+        }
+    }
+}
