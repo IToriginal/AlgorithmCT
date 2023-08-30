6d76da4517798e48dc91c4f8d5da1a41c01f65ef
IToriginal <gitshineit@gmail.com>
2023-08-30 18:35:46 +0900

    2023. 08. 30 (수)
    ✅ Inflearn: Sorting and Searching - (8 / 10)

--- README.md
+++ README.md
@@ -28,6 +28,9 @@ Start: 2023. 07. 28 (금) 22:00 <br>
 - [♻️ BOJ_1018](https://www.acmicpc.net/problem/1018)
 - [♻️ BOJ_11651](https://www.acmicpc.net/problem/11651)
 
+### 2023. 08. 30 (수)
+✅ ~~Inflearn ...  Sorting and Searching (8 / 10)~~ </br>
+
 ### 2023. 08. 29 (화)
 ✅ ~~Inflearn ...  Sorting and Searching (4 / 10)~~ </br>
 ❌ No.4: LRU </br>
---
+++ src/inflearn/_06_Sorting_Searching/_05_중복확인/MyAnswer.java
@@ -0,0 +1,23 @@
+package inflearn._06_Sorting_Searching._05_중복확인;
+
+import java.io.*;
+import java.util.*;
+
+public class MyAnswer {
+    public char solution(int[] arr) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int x : arr) set.add(x);
+        if (set.size() != arr.length) return 'D';
+        else return 'U';
+    }
+
+    public static void main(String[] args) throws IOException {
+        MyAnswer T = new MyAnswer();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        String[] input = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(input[i]);
+        System.out.println(T.solution(arr));
+    }
+}
