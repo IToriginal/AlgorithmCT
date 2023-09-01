76a3369bbe8d4fc950c42debc5e437bfc995fce9
IToriginal <gitshineit@gmail.com>
2023-09-02 08:03:26 +0900

    2023. 09. 02 (토)
    ✅ Inflearn: Sorting and Searching - (10 / 10)
    ❌ No.9: 뮤직비디오

--- README.md
+++ README.md
@@ -28,6 +28,10 @@ Start: 2023. 07. 28 (금) 22:00 <br>
 - [♻️ BOJ_1018](https://www.acmicpc.net/problem/1018)
 - [♻️ BOJ_11651](https://www.acmicpc.net/problem/11651)
 
+### 2023. 09. 02 (토)
+✅ ~~Inflearn ...  Sorting and Searching (10 / 10)~~ </br>
+❌ No.9: 뮤직비디오 </br>
+
 ### 2023. 08. 30 (수)
 ✅ ~~Inflearn ...  Sorting and Searching (8 / 10)~~ </br>
 
---
+++ src/inflearn/_06_Sorting_Searching/_09_뮤직비디오/Answer.java
@@ -0,0 +1,42 @@
+package inflearn._06_Sorting_Searching._09_뮤직비디오;
+
+import java.util.*;
+
+public class Answer {
+    public int count(int[] arr, int capacity) {
+        int cnt = 1, sum = 0;
+        for (int x : arr) {
+            if (sum + x > capacity) {
+                cnt++;
+                sum = x;
+            } else {
+                sum += x;
+            }
+        }
+        return cnt;
+    }
+    public int solution(int n, int m, int[] arr){
+        int answer = 0;
+        int lt = Arrays.stream(arr).max().getAsInt();
+        int rt = Arrays.stream(arr).sum();
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (count(arr, mid) <= m) {
+                answer = mid;
+                rt = mid - 1;
+            } else {
+                lt = mid + 1;
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args){
+        Answer T = new Answer();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int m = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = kb.nextInt();
+        System.out.println(T.solution(n, m, arr));
+    }
+}
