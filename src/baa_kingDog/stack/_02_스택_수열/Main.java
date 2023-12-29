86bb9001b025de76b821e18a15c3a5b07550a722
IToriginal <gitshineit@gmail.com>
2023-12-29 17:16:53 +0900

    ✅ [BOJ] Stack
    문제 이해에 어려움을 겪었다.
    스택에 넣는 숫자값을 start 변수로 선언하고 활용하는데까지의 생각이 오래걸리고 어려움을 겪었다.
    백준 기준 등급에 비해서 문제의 이해부터 당황하게 했던 문제였다.
    
    * 자바(Java) 알고리즘 코딩테스트 대비: 바킹독 정복

---
+++ src/baa_kingDog/stack/_02_스택_수열/Main.java
@@ -0,0 +1,57 @@
+package baa_kingDog.stack._02_스택_수열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+
+    private StringBuilder result;
+    private Stack<Integer> stack;
+
+    private void popNumber() {
+        stack.pop();
+        result.append("-").append("\n");
+    }
+
+    private void pushNumber(int target, int start) {
+        for (int num = start + 1; num <= target; num++) {
+            stack.push(num);
+            result.append("+").append("\n");
+        }
+    }
+
+    private String solution(int n, int[] data) {
+        result = new StringBuilder();
+        stack = new Stack<>();
+
+        int start = 0, index = 0;
+        while (n-- > 0) {
+            int target = data[index];
+            index++;
+
+            if (target > start) {
+                pushNumber(target, start);
+                start = target;
+            } else if (stack.peek() != target) {
+                return "NO";
+            }
+            popNumber();
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main m = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] data = new int[n];
+        for (int i = 0; i < n; i++) {
+            data[i] = Integer.parseInt(br.readLine());
+        }
+        System.out.println(m.solution(n, data));
+    }
+
+}
