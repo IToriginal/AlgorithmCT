2997b19524fc41405c487b327ab2f494f80b2fd5
IToriginal <gitshineit@gmail.com>
2023-12-29 17:18:05 +0900

    ✅ [BOJ] Stack
    문제 이해는 쉬웠다. 구현이 생각처럼 잘되지 않아서 스택에 쌓는 데이터의 기준을 선정하는데 어려움을 겪게 되었다.
    
    건물의 번호는 1부터 시작하는데 배열의 인덱스는 0부터 구현해서 한번씩 헷갈려서 구현에 어려움을 겪게 되었다.
    
    어떤 데이터를 가지고 스택에 쌓고 구현할지가 중요했던 문제로 생각한다.
    
    * 자바(Java) 알고리즘 코딩테스트 대비: 바킹독 정복

---
+++ src/baa_kingDog/stack/_03_탑/Main.java
@@ -0,0 +1,40 @@
+package baa_kingDog.stack._03_탑; // BOJ2493 - G5
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Stack;
+
+public class Main {
+
+    private int[] solution(int n, int[] heights) {
+        int[] result = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            // 스택이 비어있지 않고 현재 탑의 높이가 스택의 맨 위 탑의 높이보다 큰 경우:
+            // 스택에서 맨 위의 탑을 제거 (현재 탑보다 큰 탑을 찾기 위함)
+            while (!stack.isEmpty() && heights[stack.peek()] < heights[i]) {
+                stack.pop();
+            }
+            result[i] = stack.isEmpty() ? 0 : stack.peek() + 1;
+            stack.push(i);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main m = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] heights = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        for (int h : m.solution(n, heights)) {
+            System.out.print(h +" ");
+        }
+    }
+
+}
