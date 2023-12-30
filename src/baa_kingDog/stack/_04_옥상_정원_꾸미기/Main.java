c87f7d2fec3d90a17f0797637edda24e484ce15a
IToriginal <gitshineit@gmail.com>
2023-12-30 16:26:15 +0900

    ✅ [BOJ] Stack
    2493 탑문제와 비슷한 유형의 문제라고 판단했다. 탑 문제를 풀어서 인지 문제 풀이에 대한 접근은 쉬웠다고 생각한다.
    하지만, 문제가 비슷하다 하여 문제에 제시되어 있는 조건을 꼼꼼하게 읽지 못했던 부분에 어려움을 겪었다. (long 타입, 같은 높이의 건물이 존재한다.)
    
    * 자바(Java) 알고리즘 코딩테스트 대비: 바킹독 정복

---
+++ src/baa_kingDog/stack/_04_옥상_정원_꾸미기/Main.java
@@ -0,0 +1,36 @@
+package baa_kingDog.stack._04_옥상_정원_꾸미기; // BOJ6198 - G5
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+
+    private long solution(int n, int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+
+        long sum = 0;
+        for (int index = 0; index < n; index++) {
+            while (!stack.isEmpty() && heights[stack.peek()] <= heights[index]) {
+               stack.pop();
+            }
+            sum += stack.size();
+            stack.push(index);
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main m = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] heights = new int[n];
+        for (int i = 0; i < n; i++) {
+            heights[i] = Integer.parseInt(br.readLine());
+        }
+        System.out.println(m.solution(n, heights));
+    }
+
+}
