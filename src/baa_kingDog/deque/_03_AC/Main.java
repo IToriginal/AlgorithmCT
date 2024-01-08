844e0aaa67d8d7b4c4e9257583e75be853ce28e6
IToriginal <gitshineit@gmail.com>
2024-01-09 00:57:17 +0900

    ✅ [BOJ] 덱
    입력과 출력에 어려움을 겪었다. `[`, `]`,  `,` 이와 같이 `String` <-> `deque` 의 변환 작업에서 익숙하지 않아 문법을 찾아 힘겹게 문제를 해결할 수 있었다.
    
    * 자바(Java) 알고리즘 코딩테스트 대비: 바킹독 정복

---
+++ src/baa_kingDog/deque/_03_AC/Main.java
@@ -0,0 +1,62 @@
+package baa_kingDog.deque._03_AC; // BOJ5430 - G5
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    private String solution(String p, int n, String arrStr) {
+        Deque<Integer> deque = new LinkedList<>();
+        StringTokenizer st = new StringTokenizer(arrStr, "[],");
+        while (st.hasMoreTokens()) {
+            deque.add(Integer.parseInt(st.nextToken()));
+        }
+
+        boolean isReverse = false;
+
+        for (char func : p.toCharArray()) {
+            if (func == 'R') {
+                isReverse = !isReverse;
+            } else {
+                if (deque.isEmpty()) {
+                    return "error";
+                }
+
+                if (isReverse) {
+                    deque.removeLast();
+                } else {
+                    deque.removeFirst();
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder("[");
+        while (!deque.isEmpty()) {
+            sb.append(isReverse ? deque.removeLast() : deque.removeFirst());
+            if (!deque.isEmpty()) {
+                sb.append(',');
+            }
+        }
+        sb.append(']');
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main m = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder result = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+
+        for (int testCase = 1; testCase <= T; testCase++) {
+            String p = br.readLine();
+            int n = Integer.parseInt(br.readLine());
+            String arrStr = br.readLine();
+            result.append(m.solution(p, n, arrStr)).append("\n");
+        }
+        System.out.println(result);
+    }
+
+}
